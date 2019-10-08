1665d6ae7226c5d07aa584ceb75fa8424144dee3
Miguel Camba <miguel.camba@gmail.com>
2019-10-09 00:50:58 +0200

    Add special mode to trigger errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < JSONAPI::ResourceController
   skip_before_action :verify_authenticity_token
+  before_action :error_if_in_error_mode
+
+  private
+
+  def error_if_in_error_mode
+    if ENV["ERROR_MODE"] === "1"
+      head 400
+      return false
+    end
+  end
 end
